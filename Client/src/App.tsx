fec8e05dbbf051154019ad7ea65ffd5f5a5f937f
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-05-02 21:55:17 +0800

    Descturctured React Hooks when importing from App.tsx

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import { useDispatch } from "react-redux";
 import { Navigate, Route, Routes } from "react-router-dom";
