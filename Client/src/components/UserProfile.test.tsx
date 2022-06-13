57052cc1f70cbd44e59d4117c28aba484fcd49f5
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-06-13 20:07:42 +0800

    Fixed a bug that showed toBeInDocment not being a function

--- Client/src/components/UserProfile.test.tsx
+++ Client/src/components/UserProfile.test.tsx
@@ -6,14 +6,14 @@ describe("UserProfile", () => {
     it("Check if User Id Exists", () => {
         render(<UserProfile id="" email="" role="" />);
         const id = screen.getByText(/Id/);
-        expect(id).toBeTruthy();
+        expect(id).toBeInTheDocument();
     });
 
     it("Should Render the same User Id passed into id prop", () => {
         const myId = "My Id";
         render(<UserProfile id={myId} email="" role="" />);
         const id = screen.getByText("Id: " + myId);
-        expect(id).toBeTruthy();
+        expect(id).toBeInTheDocument();
     });
 
     // it("Check if User Name Exists", () => {
