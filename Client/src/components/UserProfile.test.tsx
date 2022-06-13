bce15217882190d2731172499ad6c52a2988ccca
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-06-13 20:07:01 +0800

    Added tests in UserProfile to Render the same User Id passed into id prop

--- Client/src/components/UserProfile.test.tsx
+++ Client/src/components/UserProfile.test.tsx
@@ -1,10 +1,23 @@
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import UserProfile from "./UserProfile";
 
 describe("UserProfile", () => {
-    it("Check if ID Exists", () => {
+    it("Check if User Id Exists", () => {
         render(<UserProfile id="" email="" role="" />);
         const id = screen.getByText(/Id/);
         expect(id).toBeTruthy();
     });
+
+    it("Should Render the same User Id passed into id prop", () => {
+        const myId = "My Id";
+        render(<UserProfile id={myId} email="" role="" />);
+        const id = screen.getByText("Id: " + myId);
+        expect(id).toBeTruthy();
+    });
+
+    // it("Check if User Name Exists", () => {
+    //     render(<UserProfile id="" email="" role="" />);
+    //     const name = screen.getByRole("")
+    // });
 });
