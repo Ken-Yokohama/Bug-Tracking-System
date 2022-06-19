ce8954fb18b6b03c452a66cfd0068d4d5aacd446
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-06-20 00:07:58 +0800

    Added MockMenuOptions to render MenuOptions inside a router

--- Client/src/components/MenuOptions.test.tsx
+++ Client/src/components/MenuOptions.test.tsx
@@ -4,17 +4,35 @@ import MenuOptions from "./MenuOptions";
 import HomeTwoToneIcon from "@mui/icons-material/HomeTwoTone";
 import { BrowserRouter as Router } from "react-router-dom";
 
+interface props {
+    link: string;
+    label: string;
+    Icon: any;
+    setToggleMenu: React.Dispatch<React.SetStateAction<Boolean>>;
+}
+
+const MockMenuOptions = ({ link, label, Icon, setToggleMenu }: props) => {
+    return (
+        <Router>
+            <MenuOptions
+                link={link}
+                label={label}
+                Icon={Icon}
+                setToggleMenu={setToggleMenu}
+            />
+        </Router>
+    );
+};
+
 describe("MenuOptions", () => {
     it("Check if User Id Exists", () => {
         render(
-            <Router>
-                <MenuOptions
-                    Icon={HomeTwoToneIcon}
-                    label=""
-                    link=""
-                    setToggleMenu={() => {}}
-                />
-            </Router>
+            <MockMenuOptions
+                Icon={HomeTwoToneIcon}
+                label=""
+                link=""
+                setToggleMenu={() => {}}
+            />
         );
         const label = screen.getByRole("heading");
         expect(label).toBeInTheDocument();
