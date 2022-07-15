791bbf8fbf7e1ab7060ff90707e21a010e879db3
Ken Yokohama <kenlopezyokohama@gmail.com>
2022-07-15 08:20:44 +0800

    Update client loading page

--- Client/src/pages/Loading.tsx
+++ Client/src/pages/Loading.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 const Loading = () => {
+
+    const handleRefresh = () => {
+        window.location.reload();
+    }
+
     return (
         <div>
-            <h1>Loading...</h1>
+            <h1>Spinning Up Backend... <br />Please Wait...</h1>
+            <br />
+            <p>This Could Take Up to 15 seconds</p>
+            <br />
+            <button onClick={handleRefresh}>Refresh Page</button>
         </div>
     );
 };
